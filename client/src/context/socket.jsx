5c2a2d9f6f504b258be1421883b25d722d9f80c0
aminxare <amnxare@gmail.com>
2023-09-19 21:38:12 +0330

    refactor socket in client

--- client/src/context/socket.jsx
+++ client/src/context/socket.jsx
@@ -1,12 +1,23 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io(process.env.REACT_APP_SOCKETIO_URL);
-
-export const socketContext = createContext({ socket });
+export const socketContext = createContext(null);
 export const useSocket = () => useContext(socketContext);
 
 const SocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(() =>
+    io(process.env.REACT_APP_SOCKETIO_URL)
+  );
+
+  // useEffect(() => {
+  //   socket.on("disconnect", () => {
+  //     console.log("socket disconnected: ", socket.id);
+  //   });
+  //   socket.on("connection", () => {
+  //     console.log("socket connected: ", socket.id);
+  //   });
+  // }, [socket]);
+
   return (
     <socketContext.Provider value={{ socket }}>
       {children}
