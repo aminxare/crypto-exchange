884792db3e6bd1f03afdd5b0f6ea548720c315d0
aminxare <amnxare@gmail.com>
2023-09-20 22:18:41 +0330

    bug fix

--- client/src/features/crypto/components/Currencies.jsx
+++ client/src/features/crypto/components/Currencies.jsx
@@ -1,13 +1,42 @@
-import React from "react";
-import { List, ListItem, ListItemText, Paper, Typography } from "@mui/material";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
 import { usePrice } from "../context/price";
 
 const CurrencyList = () => {
   const { currencies } = usePrice();
-  
+
   return (
-    <Paper elevation={3} style={{ padding: "16px", minWidth:"15em" }} >
-      <Typography variant="h6" gutterBottom>
+    <TableContainer
+      component={Paper}
+      elevation={3}
+      style={{ minWidth: "15em" }}
+    >
+      <Table>
+        <TableHead>
+          <TableRow >
+            <TableCell sx={{fontWeight: "bold"}}>Coin</TableCell>
+            <TableCell sx={{fontWeight: "bold"}}>Price</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {currencies.map((currency) => (
+            <TableRow key={currency.id}>
+              <TableCell primary={currency.name}>{currency.name}</TableCell>
+              <TableCell primary={`$${currency.price}`}>
+                {`$${currency.price}`}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {/* <Typography variant="h6" gutterBottom>
         Currency List
       </Typography>
       <List>
@@ -19,8 +48,8 @@ const CurrencyList = () => {
             <ListItemText primary={`$${currency.price}`} />
           </ListItem>
         ))}
-      </List>
-    </Paper>
+      </List> */}
+    </TableContainer>
   );
 };
 
